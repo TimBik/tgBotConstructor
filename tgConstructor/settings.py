df79cf2000b9cfea718dcc97ce1cc5160be7ceb0
TimBik <timurbikmullin@mail.ru>
2024-12-03 21:17:11 +0300

    init

--- tgConstructor/settings.py
+++ tgConstructor/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from pathlib import Path
 
 import project_settings
@@ -72,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tgConstructor.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
