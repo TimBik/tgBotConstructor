d84b4c39b81d9238041f1366461138baa7f13078
TimBik <timurbikmullin@mail.ru>
2025-01-15 19:26:39 +0300

    update

--- tgBot/handlers/message_handler.py
+++ tgBot/handlers/message_handler.py
@@ -6,8 +6,12 @@ from aiogram.utils.callback_answer import CallbackAnswer
 from asgiref.sync import sync_to_async
 
 from core.models import InlineButton
+from project_settings import MY_TG_ID
 from tgBot.app import dp, bot
 from tgBot.event_manager import run_events
+from tools.logging import get_logging
+
+logger = get_logging(__name__)
 
 
 @dp.message(F.text)
@@ -18,15 +22,20 @@ async def message_handler(msg: types.Message):
 
 @dp.callback_query()
 async def message_handler(callback: CallbackQuery):
-    user_id = callback.from_user.id
-    print(callback.data)
-    data = json.loads(callback.data)
-    inline_button_id = data['button_id']
-    inline_button = await sync_to_async(
-        InlineButton.objects.get
-    )(id=inline_button_id)
-    await bot.answer_callback_query(callback.id)
-    next_event = await sync_to_async(
-        lambda: inline_button.next_event
-    )()
-    await run_events(user_id, [next_event], callback, data)
+    try:
+        user_id = callback.from_user.id
+        print(callback.data)
+        data = json.loads(callback.data)
+        inline_button_id = data['button_id']
+        inline_button = await sync_to_async(
+            InlineButton.objects.get
+        )(id=inline_button_id)
+        await bot.answer_callback_query(callback.id)
+        next_event = await sync_to_async(
+            lambda: inline_button.next_event
+        )()
+        await run_events(user_id, [next_event], callback, data)
+    except Exception as e:
+        if MY_TG_ID:
+            await bot.send_message(chat_id=MY_TG_ID, text=e)
+        logger.exception(e)
--- tgBot/handlers/start_handler.py
+++ tgBot/handlers/start_handler.py
@@ -3,13 +3,16 @@ from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
 
 from core.models import TgEvent, TgEventType, Role
+from project_settings import MY_TG_ID
 from tgBot.app import bot
 from tgBot.event_manager import run_events
 from aiogram import types
 
 from tgBot.app import dp
+from tools.logging import get_logging
 
 User = get_user_model()
+logger = get_logging(__name__)
 
 
 async def get_or_create_user(message: types.Message):
@@ -17,8 +20,8 @@ async def get_or_create_user(message: types.Message):
         tg_id=message.from_user.id,
         defaults={
             "tg_id": message.from_user.id,
-            "username": message.from_user.username,
-            "first_name": message.from_user.first_name ,
+            "username": message.from_user.username if message.from_user.username else "",
+            "first_name": message.from_user.first_name if message.from_user.first_name else "",
             "last_name": message.from_user.last_name if message.from_user.last_name else "",
             "role": Role.AUTHORIZED,
         }
@@ -28,17 +31,23 @@ async def get_or_create_user(message: types.Message):
 
 @dp.message(Command('start'))
 async def cmd_start(message: types.Message):
-    user_tg_id = message.from_user.id
-    user = await get_or_create_user(message)
-    if user.role == Role.ADMIN:
-        await bot.send_message(chat_id=user_tg_id, text=f"Приветствую админа {user.username}")
-        return
-    if not (" " in message.text and message.text.split()[1] == "by_qr"):
-        await bot.send_message(chat_id=user_tg_id, text="Я отвечаю только тем кто пришел ко мне по QR-коду с колоды")
-        return
+    try:
+        user_tg_id = message.from_user.id
+        user = await get_or_create_user(message)
+        if user.role == Role.ADMIN:
+            await bot.send_message(chat_id=user_tg_id, text=f"Приветствую админа {user.username}")
+            return
+        if not (" " in message.text and message.text.split()[1] == "by_qr"):
+            await bot.send_message(chat_id=user_tg_id,
+                                   text="Я отвечаю только тем кто пришел ко мне по QR-коду с колоды")
+            return
 
-    events = await sync_to_async(TgEvent.objects.filter)(
-        type=TgEventType.start_event
-    )
-    events_list = await sync_to_async(list)(events)
-    await run_events(user_tg_id, events_list)
+        events = await sync_to_async(TgEvent.objects.filter)(
+            type=TgEventType.start_event
+        )
+        events_list = await sync_to_async(list)(events)
+        await run_events(user_tg_id, events_list)
+    except Exception as e:
+        if MY_TG_ID:
+            await bot.send_message(chat_id=MY_TG_ID, text=e)
+        logger.exception(e)
